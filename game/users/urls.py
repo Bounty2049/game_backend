3b8f6c625766220f844ce112fa02ae0bbc6fc835
Bounty2049 <icop2049@gmail.com>
2024-03-30 21:09:23 +0400

    added opportunity to export data from model guest to excel and csv

--- game/users/urls.py
+++ game/users/urls.py
@@ -6,4 +6,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('api/', views.GuestView.as_view(), name='api'),
+    path('api/to_excel/', views.ExportToExcel.as_view(), name='export to excel'),
+    path('api/to_csv/', views.ExportToCSV.as_view(), name='export to csv')
 ]
--- game/users/views.py
+++ game/users/views.py
@@ -1,8 +1,14 @@
-from rest_framework import generics, status
+import xlsxwriter
+import csv
+
+from django.http import HttpResponse
+
+from rest_framework import generics, views, status
 from rest_framework.response import Response
 
 from .serializers import GuestSerializer
 from .models import Guest
+
 
 
 class GuestView(generics.ListCreateAPIView):
@@ -23,3 +29,45 @@ class GuestView(generics.ListCreateAPIView):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+
+class ExportToExcel(views.APIView):
+
+    def get(self, request):
+        queryset = Guest.objects.all().values()
+
+        # Create a new Excel workbook and add a worksheet
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename="data.xlsx"'
+        workbook = xlsxwriter.Workbook(response)
+        worksheet = workbook.add_worksheet()
+
+        # Write header row
+        header_row = ['name', 'email', 'phone']  # Replace with actual field names
+        for col_num, field_name in enumerate(header_row):
+            worksheet.write(0, col_num, field_name)
+
+        # Write data rows
+        for row_num, row_data in enumerate(queryset, start=1):
+            for col_num, field_name in enumerate(header_row):
+                worksheet.write(row_num, col_num, row_data.get(field_name, ''))
+
+        workbook.close()
+        return response
+    
+
+class ExportToCSV(views.APIView):
+    def get(self, request):
+        queryset = Guest.objects.all().values()
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="data.csv"'
+
+        writer = csv.writer(response)
+        # Write header row
+        writer.writerow(['Имя', 'Почта', 'Телефон'])  # Replace 'Field1', 'Field2', ... with actual field names
+
+        for item in queryset:
+            writer.writerow([item['name'], item['email'], item['phone']])  # Replace 'field1', 'field2', ... with actual field names
+
+        return response
